c5736bd17dbac79905d86f7142185e22a3589d0b
Diego Ormaza <ormazadepaul@gmail.com>
2019-10-30 15:08:19 -0300

    fixed homepage

--- server.py
+++ server.py
@@ -4,6 +4,10 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 #decorator
+@app.route("/")
+def home():
+    return render_template('index.html')
+
 @app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
